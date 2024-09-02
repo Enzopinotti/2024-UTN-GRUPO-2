4fb8005bdd58cc9779ddac714e284e828a012b9a
Enzo Pinotti <enzopinottii@gmail.com>
2024-09-01 21:14:54 -0300

    add: Desarrollo del header en Versión Mobile
    Tamaño <800px

--- antigal.client/src/components/layout/Header.js
+++ antigal.client/src/components/layout/Header.js
@@ -5,27 +5,50 @@ import Logo from '../common/Logo';
 import CartWidget from '../common/CartWidget';
 import LupaWidget from '../common/LupaWidget';
 import MenuHamburger from '../common/MenuHamburger';
+import CloseIcon from '../common/CloseIcon';
+import SocialMedia from '../common/SocialMedia';
+import UserIcon from '../common/UserIconDos';
 
 const Header = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    if (!isMenuOpen) {
+      setIsMenuVisible(true);   
+      setIsMenuOpen(true);    
+    } else {
+      setIsMenuOpen(false);   
+    }
   };
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
+      const mobile = window.innerWidth <= 800;
+      setIsMobile(mobile);
+      if (!mobile) {
+        setIsMenuOpen(false);
+        setIsMenuVisible(false);
+      }
     };
 
     window.addEventListener('resize', handleResize);
 
-    // Cleanup function
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
+  useEffect(() => {
+    if (!isMenuOpen && isMenuVisible) {
+      const timer = setTimeout(() => {
+        setIsMenuVisible(false);
+      }, 300);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isMenuOpen, isMenuVisible]);
 
   return (
     <header className='header'>
@@ -33,11 +56,15 @@ const Header = () => {
         <div className='header-mobile'>
           <Logo />
           <MenuHamburger onClick={toggleMenu} />
-          {isMenuOpen && (
-            <div className='mobile-menu'>
-              <NavBar vertical={true} />
-              <button className='login-button'>Iniciar Sesión</button>
-              <button className='register-button'>Registrarse</button>
+          {isMenuVisible && (
+            <div className={`mobile-menu ${isMenuOpen ? 'open' : 'close'}`}>
+              <article className='cierreYlogo'>
+                <Logo />
+                <CloseIcon onClick={toggleMenu} />
+              </article>
+              <UserIcon />                  
+              <NavBar vertical={true} onLinkClick={toggleMenu} /> 
+              <SocialMedia />
             </div>
           )}
         </div>
