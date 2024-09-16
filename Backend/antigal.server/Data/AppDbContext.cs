fdacb34a2d64d596da0ff8bccce6faf24918d057
Matias Rau Bekerman <bekermanmatias@gmail.com>
2024-09-15 22:28:00 -0300

    add: clase categoria

--- Backend/antigal.server/Data/AppDbContext.cs
+++ Backend/antigal.server/Data/AppDbContext.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using antigal.server.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace antigal.server.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-    
-   
+
+        public DbSet<Categoria> Categorias { get; set; }
     }
 }
---
+++ Backend/antigal.server/Models/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace antigal.server.Models
+{
+    public class Categoria
+    {
+        
+
+        [Key]
+        public int idCategoria { get; set; }
+        public string? descripcion { get; set; }
+        public string? imagen { get; set; }
+        [Required]
+        public required string nombre { get; set; }
+
+        public Categoria(int idCategoria, string? descripcion, string? imagen, string nombre)
+                {
+                    this.idCategoria = idCategoria;
+                    this.descripcion = descripcion;
+                    this.imagen = imagen;
+                    this.nombre = nombre;
+                }
+    }
+}
