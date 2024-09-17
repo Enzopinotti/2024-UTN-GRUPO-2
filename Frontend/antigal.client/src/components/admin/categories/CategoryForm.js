3d48afbf25c12eb48958b6d8681d9b5c3d3a4208
natashaC <natashacadabon@gmail.com>
2024-09-17 17:32:57 -0300

    add: estilado del form agregar categoria

---
+++ Frontend/antigal.client/src/components/admin/categories/CategoryForm.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const CategoryForm = ({ show, onClose }) => {
+  if (!show) {
+    return null;
+  }
+  //aca iría la lógica
+
+  return (
+    <div className="form-overlay" onClick={onClose}>
+      <div className="form">
+        <div className="top-section">
+          <h2>Crear nueva categoría </h2>
+          <button type="button" className="exit-btn" onClick={onClose}>
+            <i className="fa-solid fa-xmark"></i>
+          </button>
+        </div>
+        <form>
+          <div className="input">
+            <label htmlFor="nombre">Nombre de la categoría:</label>
+            <input type="text" id="nombre" name="nombre" required />
+          </div>
+          <div className="input">
+            <label htmlFor="descripcion">Descripción:</label>
+            <textarea id="descripcion" name="descripcion" required />
+          </div>
+          <div className="input">
+            <label htmlFor="imagen">Subir imagen:</label>
+            <input type="file" id="imagen" name="imagen" accepts="image/*" />
+          </div>
+          <div className="bottom-section">
+            <button type="button" onClick={onClose}>
+              Cancelar
+            </button>
+            <button type="submit" className="create-btn">Crear</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryForm;
--- Frontend/antigal.client/src/components/admin/categories/CategoryListContainer.js
+++ Frontend/antigal.client/src/components/admin/categories/CategoryListContainer.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import CategoryList from './CategoryList';
-import AdminNav from '../AdminNav';
+import React, { useState } from "react";
+import CategoryList from "./CategoryList";
+import AdminNav from "../AdminNav";
+import CategoryForm from "./CategoryForm";
 
-const CategoryListContainer=()=>{
-    return(
+const CategoryListContainer = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  return (
     <div className="admi-page">
-        <AdminNav/>
-        <div className='new-btn'>
-            <button> + Nueva Categoría </button>
-        </div>
-        <CategoryList/>
+      <AdminNav />
+      <div className="new-btn">
+        <button onClick={handleShowModal}> + Nueva Categoría </button>
+      </div>
+
+      <CategoryForm show={showModal} onClose={handleCloseModal} />
+
+      <CategoryList />
     </div>
-);} 
+  );
+};
 export default CategoryListContainer;
