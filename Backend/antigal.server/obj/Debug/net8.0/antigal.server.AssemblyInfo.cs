202dac1705d6e3912c611a70e31207a32332c1e6
Matias Rau Bekerman <bekermanmatias@gmail.com>
2024-10-18 15:33:19 -0300

    fix: migration usuarios

--- Backend/antigal.server/obj/Debug/net8.0/antigal.server.AssemblyInfo.cs
+++ Backend/antigal.server/obj/Debug/net8.0/antigal.server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("antigal.server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4706544081ab32579b3f34fae72c46323c72d897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e6f56f1c2fc389b40f55475892ebb318ff112f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("antigal.server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("antigal.server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
