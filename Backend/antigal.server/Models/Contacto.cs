f75dfd23d2190b96c2a89865b6f6e59c889dd8f3
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-20 13:21:28 -0300

    B7b2 lab: make contact scalar contract required

--- Backend/antigal.server/Models/Contacto.cs
+++ Backend/antigal.server/Models/Contacto.cs
@@ -1,12 +1,12 @@
-﻿namespace antigal.server.Models
+namespace antigal.server.Models
 {
     public class Contacto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Asunto { get; set; }
-        public string Mensaje { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Asunto { get; set; }
+        public required string Mensaje { get; set; }
         public DateTime Fecha { get; set; }
     }
 }
