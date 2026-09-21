59b82863c269de62686c65840d202a687aaac8f1
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 12:14:56 -0300

    B42: reduce Sale repository contract to creation

--- Backend/antigal.server/Repositories/ISaleRepository.cs
+++ Backend/antigal.server/Repositories/ISaleRepository.cs
@@ -1,11 +1,9 @@
-﻿using antigal.server.Models;
+using antigal.server.Models;
 
 namespace antigal.server.Repositories
 {
     public interface ISaleRepository
     {
         Task<Sale?> CreateSaleAsync(Sale sale);
-        Task<Sale?> GetSaleByIdAsync(int idVenta);
-        Task<bool> UpdateSaleAsync(Sale sale);
     }
 }
