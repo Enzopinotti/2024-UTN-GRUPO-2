25bcf7aa5127654f7919dfe298a399b656f13551
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 12:13:53 -0300

    B42: remove dead Sale response DTO

--- Backend/antigal.server/Models/Dto/VentaDtos/SaleResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace antigal.server.Models.Dto.VentaDtos
-{
-    public class SaleResponseDto
-    {
-        public bool IsSuccess { get; set; }
-        public required string Message { get; set; }
-        public SaleDto ?Data { get; set; }
-    }
-}
